312863def943622c39baf53eab986962fd1b3ca8
Houston McClung <hmcclungiiigmail.com>
2019-05-13 13:52:25 -0400

    home and about pages have been added

---
+++ demo1/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "demo1/home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "demo1/about.html"
